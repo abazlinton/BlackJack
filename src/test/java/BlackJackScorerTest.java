ce5e1fca59bffa59196e8d0ddc8fdc099c851dfd
Alex Bazlinton <abazlinton@gmail.com>
2020-02-17 09:58:11 +0000

    Revert "simplify a test"
    This reverts commit aa206d139035700464c81a35b9748c4a1b885cdc.

--- src/test/java/BlackJackScorerTest.java
+++ src/test/java/BlackJackScorerTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.assertEquals;
 public class BlackJackScorerTest {
 
     @Test
-    public void test_one_ace_scores_11(){
+    public void test_one_ace_as_11(){
         Card ace1 = new Card(Rank.ACE, Suit.DIAMONDS);
         Card ace2 = new Card(Rank.ACE, Suit.DIAMONDS);
         ArrayList<Card> cards = new ArrayList<>();
@@ -29,16 +29,18 @@ public class BlackJackScorerTest {
     }
 
     @Test
-    public void test_all_aces_score_1(){
+    public void test_all_aces_score_one(){
         Card ace1 = new Card(Rank.ACE, Suit.DIAMONDS);
         Card ace2 = new Card(Rank.ACE, Suit.DIAMONDS);
+        Card ace3 = new Card(Rank.ACE, Suit.DIAMONDS);
         Card ten = new Card(Rank.TEN, Suit.DIAMONDS);
-        Card nine = new Card(Rank.NINE, Suit.DIAMONDS);
+        Card eight = new Card(Rank.EIGHT, Suit.DIAMONDS);
         ArrayList<Card> cards = new ArrayList<>();
         cards.add(ace1);
         cards.add(ace2);
+        cards.add(ace3);
         cards.add(ten);
-        cards.add(nine);
+        cards.add(eight);
         int score = BlackJackScorer.getHandScore(cards);
         assertEquals(21, score);
     }
